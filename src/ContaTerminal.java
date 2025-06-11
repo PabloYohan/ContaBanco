498db631767fa8b032e3bf434495870f64bf7e4b
Pablito <pabloyohandias@gmail.com>
2025-06-11 15:44:10 -0300

    Termino do primeiro desafio

--- src/ContaTerminal.java
+++ src/ContaTerminal.java
@@ -1,7 +1,21 @@
+import java.util.Scanner;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class ContaTerminal {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Por favor, digite o número da Conta !");
+        int bankAccountNumber = scanner.nextInt();
+        scanner.nextLine();
+        System.out.println("Por favor, digite o código da Agência !");
+        String bankCodeNumber = scanner.nextLine();
+        System.out.println("Por favor, digite o seu Nome !");
+        String name = scanner.nextLine();
+        System.out.println("Por favor, digite o seu Saldo !");
+        float amount = scanner.nextFloat();
 
+        System.out.printf("Olá %s, obrigado por criar uma conta em nosso banco, sua agência é %s, conta %s e seu " +
+                "saldo %s já está disponível para saque \n", name, bankCodeNumber, bankAccountNumber, amount);
     }
 }
